b0ae8ff5629e25c1b2ee7504ef5ee61f82044d68
generator-toka <fxshade@gmail.com>
2023-01-17 14:20:43 +0500

    Add files via upload

---
+++ zitect/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  plugins: [],
+};
